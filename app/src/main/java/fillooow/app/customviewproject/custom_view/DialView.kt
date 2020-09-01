dd84a2f001af1129dfc3bb018a90919025a750eb
fillooow <fil201121@yandex.ru>
2020-09-01 17:07:26 +0500

    добавил отрисовку вьюхи

--- app/src/main/java/fillooow/app/customviewproject/custom_view/DialView.kt
+++ app/src/main/java/fillooow/app/customviewproject/custom_view/DialView.kt
@@ -1,12 +1,17 @@
 package fillooow.app.customviewproject.custom_view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PointF
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 import fillooow.app.customviewproject.R
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
 
 private const val RADIUS_OFFSET_LABEL = 30
 private const val RADIUS_OFFSET_INDICATOR = -35
@@ -26,16 +31,69 @@ internal class DialView @JvmOverloads constructor(
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create( "", Typeface.BOLD)
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
 
+        radius = (min(width, height) / 2.0 * 0.8).toFloat()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        paint.color = when (fanSpeed) {
+
+            FanSpeed.OFF -> Color.GRAY
+            else -> Color.GREEN
+        }
+
+        drawFan(canvas)
+        drawFanMarker(canvas)
+        drawFanLabels(canvas)
+    }
+
+    // Draw the dial
+    private fun drawFan(canvas: Canvas) {
+
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
+    }
+
+    // Draw the indicator circle.
+    private fun drawFanMarker(canvas: Canvas) {
+
+        val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+        pointPosition.computeXYForSpeed(fanSpeed, markerRadius)
+        paint.color = Color.BLACK
+        canvas.drawCircle(pointPosition.x, pointPosition.y, radius/12, paint)
+    }
+
+    // Draw the text labels.
+    private fun drawFanLabels(canvas: Canvas) {
+
+        val labelRadius = radius + RADIUS_OFFSET_LABEL
+        for (i in FanSpeed.values()) {
+            pointPosition.computeXYForSpeed(i, labelRadius)
+            val label = resources.getString(i.label)
+            canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
+        }
+    }
+
+    private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
+        // Angles are in radians.
+        val startAngle = Math.PI * (9 / 8.0)
+        val angle = startAngle + pos.ordinal * (Math.PI / 4)
+        x = (radius * cos(angle)).toFloat() + width / 2
+        y = (radius * sin(angle)).toFloat() + height / 2
+    }
 }
 
 private enum class FanSpeed(val label: Int) {
+
     OFF(R.string.fan_off),
     LOW(R.string.fan_low),
     MEDIUM(R.string.fan_medium),
